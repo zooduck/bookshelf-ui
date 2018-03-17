5987509f42891bba7c4fd5a49435d356ee8bd2ff
Nicola Ferri <ferrinicola86@gmail.com>
2018-03-17 12:57:07 +0000

    Remove info:
    * isbn
    * description

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -64,16 +64,16 @@ const addBookToDOM = (data) => {
       book__el__thumbnail.style.backgroundImage = `url("${data.google_thumbnail}")`;
   }
   book__el__title.innerHTML = data.title;
-  book__el__isbn.innerHTML = data.isbn;
+  // book__el__isbn.innerHTML = data.isbn;
 
   book__el__authors.innerHTML = data.authors.map( (item) => item.name).join(", ");
 
   book__el__category.innerHTML = data.category;
-
-  if (data.description) {
-    book__el__description.value = data.description;
-    book__el__description.rows = data.description.length < 200? "1" : data.description.length < 400? "2" : data.description.length < 600? "3" : "6";
-  } else book__el__description.hidden = true;
+  //
+  // if (data.description) {
+  //   book__el__description.value = data.description;
+  //   book__el__description.rows = data.description.length < 200? "1" : data.description.length < 400? "2" : data.description.length < 600? "3" : "6";
+  // } else book__el__description.hidden = true;
 
   books.appendChild(book__el);
 }
