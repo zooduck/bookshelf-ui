38daf6ec6119932c7825d10a14212937970edd4c
ZOODUCK <zooduck@beniceoreatsomerice.com>
2018-04-04 17:32:15 +0100

    Update book model.
    When google api returns a book with no industryIdentifiers property, set
    the isbn property to an empty string.

--- src/scripts/models/book.model.js
+++ src/scripts/models/book.model.js
@@ -3,10 +3,15 @@ export function Book(data) {
   for (let i in data) {
     _this[i] = data[i];
   }
-  const isbns = Array.from(this.volumeInfo.industryIdentifiers);
-  const isbn13 = isbns.find( (item) => item.type == "ISBN_13");
-  const isbn = isbn13? isbn13.identifier : isbns[0].identifier;
-  _this.volumeInfo.isbn = isbn;
+  try {
+    const isbns = Array.from(this.volumeInfo.industryIdentifiers);
+    const isbn13 = isbns.find( (item) => item.type == "ISBN_13");
+    const isbn = isbn13? isbn13.identifier : isbns[0].identifier;
+    _this.volumeInfo.isbn = isbn;
+  } catch (e) {
+    _this.volumeInfo.isbn = "";
+  }
+
   try {
     _this.volumeInfo.google_thumbnail = _this.volumeInfo.imageLinks.thumbnail;
   } catch (e) {
